c4ac5f03e8657c8a4241a0794e30f583910a9ad0
romelx23 <romelx23@gmail.com>
2025-06-26 15:57:10 -0500

    add nats config

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,16 +4,22 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVERS: string;
 }
 
 const envsSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true); // Allow additional properties
 
-const { error, value } = envsSchema.validate(process.env);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const { error, value } = envsSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
@@ -24,4 +30,5 @@ const envsVars: EnvVars = value;
 export const envs = {
   port: envsVars.PORT,
   databaseUrl: envsVars.DATABASE_URL,
+  natsServers: envsVars.NATS_SERVERS,
 };
